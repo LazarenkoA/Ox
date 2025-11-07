977a652e5e44465db1cb3905f8b58f438d2a14bc
Артем <laz-mail@mail.ru>
2025-11-07 12:33:36 +0300

    пример

--- example scripts/bsp.spec.js
+++ example scripts/bsp.spec.js
@@ -1,82 +1,133 @@
 import { test, expect } from '@playwright/test';
 
-test('Тест БСП', async ({ page }) => {
-    await page.goto('http://localhost/bsp/ru_RU/');
-    await page.locator('#userName').click();
-    await page.locator('#userName').fill('Администратор');
-    await page.locator('#userName').press('Tab');
-    await page.locator('#userPassword').fill('123');
-    await page.locator('#userPassword').press('Enter');
+// тесты выполняются последовательно
+test.describe.configure({ mode: 'serial' });
 
-    await page.locator('#themesCell_theme_3').getByText('Сервисные подсистемы').click();
-    await page.locator('#cmd_0_6_txt').click();
-    await page.locator('div:nth-child(4) > div:nth-child(2) > .gridBoxTitle').click();
-    await page.getByText('АОЗТ Лабан').dblclick();
-    await closeButton(page, 'ФормаЗаписатьИЗакрыть')
-    await page.locator('[id="form4_ФормаСоздать"]').click();
-    await page.getByRole('textbox', { name: 'Наименование:' }).click();
+test.describe('Тест БСП', () => {
+    // Выполняется один раз перед всеми тестами в группе
+    test.beforeAll(async ({ browser }) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
 
-    const name = randomString();
-    await page.getByRole('textbox', { name: 'Наименование:' }).pressSequentially(name);
-    await page.getByRole('textbox', { name: 'Наименование полное:' }).click();
-    await page.getByRole('textbox', { name: 'Наименование полное:' }).pressSequentially(randomString());
-    await page.locator('[id="form6_Родитель_DLB"]').click();
-    await page.getByText('Показать все').first().click();
-    await page.locator('div:nth-child(2) > .gridBox.gridBoxTree > .gridBoxImg > div:nth-child(2)').click();
-    await page.getByText('Демо: КонтрагентыБез партнера').click();
-    await page.locator('[id="grid_form7_Список"]').getByText('Без партнера').click();
-    await page.locator('[id="form7_ФормаВыбрать"]').click();
-    await closeButton(page, 'ФормаЗаписатьИЗакрыть')
+        await page.goto('http://localhost/bsp/ru_RU/');
+        await page.locator('#userName').fill('Администратор');
+        await page.locator('#userPassword').fill('123');
+        await page.getByRole('button', { name: 'Войти' }).click();
 
-    // Содержит подстроку
-    // [id*="ФормаЗаписатьИЗакрыть"]
-    // Заканчивается на
-    // [id$="ФормаЗаписатьИЗакрыть"]
-    // Начинается с
-    // [id^="ФормаЗаписатьИЗакрыть"]
-    // Точное совпадение
-    // [id="form7_ФормаЗаписатьИЗакрыть"]
+        // Сохраняем страницу для последующих тестов
+        test.info().annotations.push({ type: 'page', description: 'shared' });
+        global.page = page;
+    });
 
-    await page.locator('[id="grid_form4_Список"]').getByText(name).click({ button: 'right' });
-    await page.locator('#popupItem4').getByText('Пометить на удаление / Снять пометку').click();
-    await page.locator('[id^="form"][id$="_Button0"] a').filter({ hasText: 'Да' }).click();
-    await page.locator('#themesCell_theme_5').getByText('Интегрируемые').click();
-    await page.locator('#cmd_0_6_txt').click();
+    test.afterAll(async () => {
+        await global.page?.close();
+    });
 
-    // клик по строке в списке
-    await page.locator('div.gridLine').last().dblclick();
-    await closeButton(page, 'ФормаПровестиИЗакрыть')
 
-    // создать документ
-    await page.locator('a[id^="form"][id$="_ФормаСоздать"]').last().click();
-    await page.locator('[id^="form"][id$="_Организация_DLB"]').click();
-    await page.getByText('Показать все').first().click();
-    await page.locator('[id^="grid_form"][id$="_Список"]').last().getByText('Перспектива ЗАО').dblclick();
-    await page.getByRole('textbox', { name: 'Сумма документа: Выбрать (F4)' }).click();
-    await page.getByRole('textbox', { name: 'Сумма документа: Выбрать (F4)' }).pressSequentially("1");
-    await page.locator('[id^="form"][id$="_Партнер_DLB"]').click();
-    await page.getByText('Показать все').first().click();
-    await page.locator('[id^="grid_form"][id$="_Список"]').last().getByText('ООО "Альфа"').dblclick();
+    test('отчет анализ активности пользователе', async () => {
+        await global.page.locator('[id$="themesCell"]').getByText('Администрирование').click();
+        await global.page.locator('#funcPanel_container').getByText('Отчеты администратора').click();
+        await global.page.locator('[id$="_Основное_div"]').getByText('Анализ активности пользователей').click();
+        await buttonClick(global.page, 'СформироватьОтчет');
+        await global.page.waitForTimeout(1000);
+        await closeForm(global.page);
+    });
+    test('открыть валюты', async () => {
+        test.setTimeout(60_000); // секунд только для этого теста
 
-    await closeButton(page, 'ФормаПровестиИЗакрыть')
+        await global.page.locator('[id$="themesCell"]').getByText('Справочники').click();
+        await global.page.locator('#funcPanel_container').locator('.functionItemBox').getByText('Валюты').click();
+        await global.page.waitForTimeout(100);
+        await doubleClickRandomRow(global.page);
+        await buttonClick(global.page, 'ФормаЗаписатьИЗакрыть');
 
-    // отчет
-    await page.locator('#themesCell_theme_11').getByText('Администрирование').click();
-    await page.getByText('Отчеты администратора').click();
-    await page.getByText('Анализ активности пользователей').click();
-    await page.locator('[id^="form"][id$="_КомпоновщикНастроекПользовательскиеНастройкиЭлемент0Значение_CB"]').last().click();
-    await page.locator('[id^="grid_form"][id$="_ПользователиСписок"]').last().getByText('Петрищев Олег Константинович (руководитель)').dblclick();
-    await page.locator('a').filter({ hasText: 'Подобрать и закрыть' }).click();
-    await page.locator('[id^="form"][id$="_СформироватьОтчет"]').last().click();
-    await page.locator('[id^="VW_page"][id$="headerTopLine_cmd_CloseButton"]').last().click();
+        const count = await global.page.locator('id$="_CommandButtonOK"');
+        if(count > 0) {
+            await global.page.locator('id$="_CommandButtonOK"').click();
+            await closeButton(global.page, 'ФормаЗаписатьИЗакрыть')
+        }
+    });
+    test('создать документ', async () => {
+        await global.page.locator('[id$="themesCell"]').getByText('подсистемы (часть 2)').click();
+        await global.page.locator('#funcPanel_container').locator('#cmd_0_6_txt').click();
+        await buttonClick(global.page, 'ФормаСоздать');
+
+        const name = randomString();
+        await global.page.getByRole('textbox', { name: 'Сумма документа' }).pressSequentially("1212");
+        await global.page.locator('[id^="form"]').locator('[id$="_Партнер_DLB"]').last().click();
+        await global.page.getByText('Показать все').first().click();
+        await global.page.waitForTimeout(100);
+        await global.page.locator('[id^="grid_"]').getByText('Пилигрим ООО').last().dblclick({ timeout: 500 });
+        await global.page.locator('[id^="form"]').locator('[id$="_Организация_DLB"]').last().click();
+        await global.page.getByText('Показать все').first().click();
+        await global.page.waitForTimeout(100);
+        await global.page.locator('[id^="grid_"]').getByText('Новые технологии ООО').last().dblclick({ timeout: 500 });
+        await buttonClick(global.page, 'ФормаПровестиИЗакрыть')
+    });
 });
+
+
+// Содержит подстроку
+// [id*="ФормаЗаписатьИЗакрыть"]
+// Заканчивается на
+// [id$="ФормаЗаписатьИЗакрыть"]
+// Начинается с
+// [id^="ФормаЗаписатьИЗакрыть"]
+// Точное совпадение
+// [id="form7_ФормаЗаписатьИЗакрыть"]
+
+
+// ---------- вспомогательные функции ----------
 
 function randomString() {
     return Math.random().toString(36).substring(2, 10);
 }
 
-async function closeButton(page, name) {
-    const elems = await page.locator(`[id$="_${name}"]`).elementHandles();
+async function closeForm(page) {
+    try {
+        await page.locator('[id^="VW_page"][id$="headerTopLine_cmd_CloseButton"]').last().click({ timeout: 500 });
+    } catch (error) {
+        console.warn('⚠️ Кнопка закрытия не найдена или не кликабельна:', error.message);
+    }
+}
+
+async function doubleClickRandomRow(page){
+    // Получаем все строки
+    const rows = await page.locator('div.gridBody div.gridLine').all();
+    if (rows.length === 0) {
+        console.log(`В списке нет строк`);
+        return
+    }
+
+    await page.waitForTimeout(200);
+
+    // Фильтруем только видимые строки
+    const visibleRows = [];
+    for (const row of rows) {
+        if (await row.isVisible()) {
+            // const text = await row.innerText();
+            // console.log(text);
+            visibleRows.push(row);
+        }
+    }
+
+    // Выбираем случайную из видимых
+    const randomIndex = Math.floor(Math.random() * visibleRows.length);
+    const targetRow = visibleRows[randomIndex];
+
+    // Двойной клик
+    await targetRow.dblclick({ timeout: 5000 });
+
+    console.log(`Выполнен двойной клик на строку с индексом: ${randomIndex} из ${rows.length}`);
+}
+
+async function buttonClick(page, name) {
+    const elem = await page.locator(`a[id$="_${name}"]`);
+    await elem.waitFor();
+
+    const elems = await elem.elementHandles();
+
+    console.log(name, elems.length)
 
     for (const btn of elems) {
         const id = await btn.evaluate(el => el.id);
